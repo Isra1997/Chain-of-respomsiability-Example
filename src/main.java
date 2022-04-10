bfecd8fb72cd4684afc8118ed38553ad0650edd2
Isra1997 <isrargheb@gmail.com>
2022-04-10 14:05:48 +0200

    add: added a unclean example.

--- src/main.java
+++ src/main.java
@@ -2,13 +2,16 @@ import java.util.UUID;
 
 public class main {
   public static void main(String[] args) {
-    calculateTotalAmountWithChainOfResponsibility();
+    //create a request
+    Invoice invoice = new Invoice(100,UUID.randomUUID(),true);
+
+    calculateTotalAmountWithChainOfResponsibility(invoice);
+    calculateTotalAmountWithOutChainOfResponsibility(invoice);
 
   }
 
-  public static void calculateTotalAmountWithChainOfResponsibility(){
-    //create a request
-    Invoice invoice = new Invoice(100,UUID.randomUUID(),true);
+  public static void calculateTotalAmountWithChainOfResponsibility(Invoice invoice){
+
 
     //initializing  processors
     VAT vat = new VAT();
@@ -24,8 +27,17 @@ public class main {
   }
 
 
-  public static void calculateTotalAmountWithOutChainOfResponsibility(){
-
+  public static void calculateTotalAmountWithOutChainOfResponsibility(Invoice invoice){
+    invoice.setTax(invoice.getAmount()*0.14);
+    invoice.setTotalAmount(invoice.getAmount() + invoice.getTax());
+    System.out.println("Vat tax:" + invoice.getTotalAmount());
+    if(invoice.isDineIn()){
+      invoice.setTotalAmount(invoice.getTotalAmount()+ invoice.getAmount()*0.12);
+      System.out.println("Service :" + invoice.getTotalAmount());
+    }else if(invoice.getAmount() > 200){
+      invoice.setTotalAmount(invoice.getTotalAmount()+ invoice.getAmount()*0.20);
+      System.out.println("Tips:" + invoice.getTotalAmount());
+    }
 
   }
 
